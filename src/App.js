42a0883ab6bfa4a6fe9e97389b3efe65e221fd98
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-10-24 22:01:00 -0300

    feat: Import Pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Provider from './context/Provider';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
+import ProductDetails from './pages/ProductDetails';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -9,9 +12,9 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
-          {/* <Route exact path="/shoppingcart" component={ ShoppingCart } />
+          <Route exact path="/shoppingcart" component={ ShoppingCart } />
           <Route exact path="/productdetails/:id" component={ ProductDetails } />
-          <Route exact path="/checkout" component={ Checkout } /> */}
+          <Route exact path="/checkout" component={ Checkout } />
         </Switch>
       </BrowserRouter>
     </Provider>
