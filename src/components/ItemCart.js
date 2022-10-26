30215ead96b2d18fbb806782d9d25b296b695108
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-10-26 16:49:16 -0300

    refactor: Add new restrictions for functions

--- src/components/ItemCart.js
+++ src/components/ItemCart.js
@@ -4,47 +4,60 @@ import StoreContext from '../context/StoreContext';
 import { setLocalStorage, getLocalStorage } from '../services/storage';
 
 function ItemCart({ product }) {
-  const { cartItems, setCartItems } = useContext(StoreContext);
+  const { cartItems, setCartItems, setWarningQuantityChange } = useContext(StoreContext);
   const [quantity, setQuantity] = useState(1);
 
-  const quantityItem = !getLocalStorage('quantity') ? [] : getLocalStorage('quantity');
+  const checkAndReturn = (check, yes, not) => {
+    const value = check ? yes : not;
+    return value;
+  };
+
+  const quantityItem = getLocalStorage('quantity');
+
+  const warningChange = () => setWarningQuantityChange((prev) => prev + 1);
 
   useEffect(() => {
-    const qtd = quantityItem.filter((item) => item.id === product.id);
+    const checkQuantityItems = checkAndReturn(quantityItem, quantityItem, []);
+    const qtd = checkQuantityItems.filter((item) => item.id === product.id);
 
     return qtd[0] && setQuantity(qtd[0].quantity);
   }, []);
 
   const addProductQuantity = () => {
     if (quantityItem.find((item) => item.id === product.id)) {
-      const newQuantity = quantityItem.map((item) => {
-        if (item.id === product.id) {
-          return { id: product.id, quantity: quantity + 1 };
-        }
-        return item;
+      const newQuantity = quantityItem.map((item2) => {
+        const updateQuantity = item2.id === product.id
+          ? { id: product.id, quantity: quantity + 1 } : item2;
+
+        return updateQuantity;
       });
 
-      setLocalStorage('quantity', newQuantity);
-    } else {
-      setLocalStorage('quantity', [...quantityItem,
-        { id: product.id, quantity: quantity + 1 }]);
+      return setLocalStorage('quantity', newQuantity);
     }
+
+    setLocalStorage('quantity', [...quantityItem,
+      { id: product.id, quantity: quantity + 1 }]);
   };
 
   const reduceProductQuantity = () => {
     const newQuantity = quantityItem.map((item) => {
-      if (item.id === product.id) {
-        return { id: product.id, quantity: quantity - 1 };
-      }
-      return item;
+      const updateQuantity = item.id === product.id
+        ? { id: product.id, quantity: quantity - 1 } : item;
+
+      return updateQuantity;
     });
 
     setLocalStorage('quantity', newQuantity);
   };
 
   const handleIncreseClick = () => {
+    if (product.available_quantity === quantity) {
+      return global.alert('Limite em estoque atingido');
+    }
+
     setQuantity((prev) => prev + 1);
     addProductQuantity();
+    warningChange();
   };
 
   const handleDecreseClick = () => {
@@ -55,12 +68,17 @@ function ItemCart({ product }) {
       reduceProductQuantity();
       return prev - 1;
     });
+    warningChange();
   };
 
   const handleDelete = () => {
     const newCartItems = [...cartItems].filter((item) => item.id !== product.id);
+    const updateQuantity = getLocalStorage('quantity')
+      .filter((item) => item.id !== product.id);
+
     setCartItems(newCartItems);
     setLocalStorage('productsAtCart', newCartItems);
+    setLocalStorage('quantity', updateQuantity);
   };
 
   return (
@@ -107,6 +125,7 @@ ItemCart.propTypes = {
     thumbnail: PropTypes.string,
     title: PropTypes.string,
     id: PropTypes.string,
+    available_quantity: PropTypes.number,
   }).isRequired,
 };
 
