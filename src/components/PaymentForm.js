7457766805751bed6ca16a4d1a99ee6d7152c8f9
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-06 23:31:11 -0300

    style: Stylize Payment-form Component

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import StoreContext from '../context/StoreContext';
+import '../styles/PaymentForm.css';
 
 function PaymentForm() {
   const { setCartItems } = useContext(StoreContext);
@@ -39,135 +40,153 @@ function PaymentForm() {
   const isDisabled = Object.values(paymentData).some((value) => value === '');
 
   return (
-    <form>
-      <label htmlFor="name">
-        Nome Completo
-        <input
-          type="text"
-          id="name"
-          name="name"
-          data-testid="checkout-fullname"
-          value={ paymentData.name }
-          onChange={ handleChange }
-        />
-      </label>
+    <form className="payment-form-container">
+      <h2>Informações do comprador</h2>
 
-      <label htmlFor="email">
-        Email
-        <input
-          type="text"
-          id="email"
-          name="email"
-          data-testid="checkout-email"
-          value={ paymentData.email }
-          onChange={ handleChange }
-        />
-      </label>
+      <div className="payment-info-container">
+        <label htmlFor="name">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-control"
+            placeholder="Nome Completo"
+            data-testid="checkout-fullname"
+            value={ paymentData.name }
+            onChange={ handleChange }
+          />
+        </label>
 
-      <label htmlFor="cpf">
-        CPF
-        <input
-          type="text"
-          id="cpf"
-          name="cpf"
-          data-testid="checkout-cpf"
-          value={ paymentData.cpf }
-          onChange={ handleChange }
-        />
-      </label>
+        <label htmlFor="cpf">
+          <input
+            type="text"
+            id="cpf"
+            name="cpf"
+            className="form-control"
+            placeholder="CPF"
+            data-testid="checkout-cpf"
+            value={ paymentData.cpf }
+            onChange={ handleChange }
+          />
+        </label>
+      </div>
 
-      <label htmlFor="phone">
-        Telefone
-        <input
-          type="text"
-          id="phone"
-          name="phone"
-          data-testid="checkout-phone"
-          value={ paymentData.phone }
-          onChange={ handleChange }
-        />
-      </label>
+      <div className="payment-info-container">
+        <label htmlFor="email">
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="form-control"
+            placeholder="Email"
+            data-testid="checkout-email"
+            value={ paymentData.email }
+            onChange={ handleChange }
+          />
+        </label>
 
-      <label htmlFor="cep">
-        CEP
-        <input
-          type="text"
-          id="cep"
-          name="cep"
-          data-testid="checkout-cep"
-          value={ paymentData.cep }
-          onChange={ handleChange }
-        />
-      </label>
+        <label htmlFor="phone">
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            className="form-control"
+            placeholder="Telefone"
+            data-testid="checkout-phone"
+            value={ paymentData.phone }
+            onChange={ handleChange }
+          />
+        </label>
+      </div>
 
-      <label htmlFor="address">
-        Endereço
-        <input
-          type="text"
-          id="address"
-          name="address"
-          data-testid="checkout-address"
-          value={ paymentData.address }
-          onChange={ handleChange }
-        />
-      </label>
-      <label htmlFor="ticket">
-        Boleto
-        <input
-          type="radio"
-          id="ticket"
-          data-testid="ticket-payment"
-          name="method"
-          onChange={ handleChange }
-          value="ticket"
-          checked={ paymentData.method === 'ticket' }
-        />
-      </label>
+      <div className="payment-info-container">
+        <label htmlFor="address" className="user-address">
+          <input
+            type="text"
+            id="address"
+            name="address"
+            className="form-control"
+            placeholder="Endereço"
+            data-testid="checkout-address"
+            value={ paymentData.address }
+            onChange={ handleChange }
+          />
+        </label>
 
-      <label htmlFor="visa">
-        Visa
-        <input
-          type="radio"
-          id="visa"
-          data-testid="visa-payment"
-          name="method"
-          onChange={ handleChange }
-          value="visa"
-          checked={ paymentData.method === 'visa' }
-        />
-      </label>
+        <label htmlFor="cep" className="user-cep">
+          <input
+            type="text"
+            id="cep"
+            name="cep"
+            className="form-control"
+            placeholder="CEP"
+            data-testid="checkout-cep"
+            value={ paymentData.cep }
+            onChange={ handleChange }
+          />
+        </label>
+      </div>
 
-      <label htmlFor="master">
-        Master
-        <input
-          type="radio"
-          id="master"
-          data-testid="master-payment"
-          name="method"
-          onChange={ handleChange }
-          value="master"
-          checked={ paymentData.method === 'master' }
-        />
-      </label>
+      <div className="payment-info-container">
+        <label htmlFor="ticket">
+          Boleto
+          <input
+            type="radio"
+            id="ticket"
+            data-testid="ticket-payment"
+            name="method"
+            onChange={ handleChange }
+            value="ticket"
+            checked={ paymentData.method === 'ticket' }
+          />
+        </label>
 
-      <label htmlFor="elo">
-        elo
-        <input
-          type="radio"
-          id="elo"
-          data-testid="elo-payment"
-          name="method"
-          onChange={ handleChange }
-          value="elo"
-          checked={ paymentData.method === 'elo' }
-        />
-      </label>
+        <label htmlFor="visa">
+          Visa
+          <input
+            type="radio"
+            id="visa"
+            data-testid="visa-payment"
+            name="method"
+            onChange={ handleChange }
+            value="visa"
+            checked={ paymentData.method === 'visa' }
+          />
+        </label>
+
+        <label htmlFor="master">
+          Master
+          <input
+            type="radio"
+            id="master"
+            data-testid="master-payment"
+            name="method"
+            onChange={ handleChange }
+            value="master"
+            checked={ paymentData.method === 'master' }
+          />
+        </label>
+
+        <label htmlFor="elo">
+          elo
+          <input
+            type="radio"
+            id="elo"
+            data-testid="elo-payment"
+            name="method"
+            onChange={ handleChange }
+            value="elo"
+            checked={ paymentData.method === 'elo' }
+          />
+        </label>
+      </div>
 
       <button
         type="button"
         data-testid="checkout-btn"
         disabled={ isDisabled }
         onClick={ handleClick }
+        className="btn btn-success finish-shop-btn"
       >
         Finalizar pedido
       </button>
